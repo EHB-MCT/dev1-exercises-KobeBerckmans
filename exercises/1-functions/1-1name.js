a0b1bcb7190a52d06a664f7349a20937db53993d
“Kobe <kobe.berckmans@student.ehb.be>
2022-10-05 17:13:33 +0200

    Einde oefening

--- exercises/1-functions/1-1name.js
+++ exercises/1-functions/1-1name.js
@@ -37,6 +37,46 @@ function drawname(){
     context.moveTo(270,250);
     context.lineTo(175,250);
     context.stroke();
+    //letter b
+    context.strokeStyle = 'pink';
+    context.beginPath();
+    context.moveTo(300,50);
+    context.lineTo(300,250);
+    context.stroke();
+    context.beginPath();
+    context.moveTo(300,155);
+    context.lineTo(400,155);
+    context.stroke();
+    context.beginPath();
+    context.moveTo(400,155);
+    context.lineTo(400,250);
+    context.stroke();
+    context.beginPath();
+    context.moveTo(400,250);
+    context.lineTo(300,250);
+    context.stroke();
+    //letter e
+    context.strokeStyle = 'green';
+    context.beginPath();
+    context.moveTo(450,50);
+    context.lineTo(450,250);
+    context.stroke();
+    context.beginPath();
+    context.moveTo(450,50);
+    context.lineTo(550,50);
+    context.stroke();
+    context.beginPath();
+    context.moveTo(450,150);
+    context.lineTo(550,150);
+    context.stroke();
+    context.beginPath();
+    context.moveTo(450,250);
+    context.lineTo(550,250);
+    context.stroke();
+    
+
+
+
 
 
 
