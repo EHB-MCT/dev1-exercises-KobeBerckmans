c91bb34a182b0564e64b4395f7728315f28d9444
“Kobe <kobe.berckmans@student.ehb.be>
2022-10-12 17:17:40 +0200

    exercise 2-1morelines

---
+++ exercises/1-functions/2-1morelines.js
@@ -0,0 +1,74 @@
+"use strict";
+let canvas = document.querySelector("canvas");
+canvas.width = window.innerWidth;
+canvas.height = window.innerHeight;
+let context = canvas.getContext("2d");
+drawLines(); 
+function drawLines(){
+
+let marginX= 50;
+
+let marginY = 50;
+let marginRight = window.innerWidth-marginX;
+
+let marginBottom = window.innerHeight-marginY;
+
+let marginBetween = (window.innerHeight- (marginY*2))/5;
+
+context.beginPath();
+    context.moveTo(marginX,marginY);
+    context.lineTo(marginRight,marginY);
+    context.stroke();
+    context.beginPath();
+    context.moveTo(marginRight,marginY);
+    context.lineTo(marginX,marginY+marginBetween);
+    context.stroke();
+    context.beginPath();
+    context.moveTo(marginX,marginY+marginBetween);
+    context.lineTo(marginRight,marginY+marginBetween);
+    context.stroke();
+    context.beginPath();
+    context.moveTo(marginRight,marginY+marginBetween);
+    context.lineTo(marginX,(marginY+marginBetween*2));
+    context.stroke();
+    context.beginPath();
+    context.moveTo(marginX,marginY+marginBetween*2);
+    context.lineTo(marginRight,marginY+marginBetween*2);
+    context.stroke();
+    context.beginPath();
+    context.moveTo(marginRight,marginY+marginBetween*2);
+    context.lineTo(marginX,(marginY+marginBetween*3));
+    context.stroke();
+    context.beginPath();
+    context.moveTo(marginX,(marginY+marginBetween*3));
+    context.lineTo(marginRight,marginY+marginBetween*3);
+    context.stroke();
+    context.beginPath();
+    context.moveTo(marginRight,marginY+marginBetween*3);
+    context.lineTo(marginX,(marginY+marginBetween*4));
+    context.stroke();
+    context.beginPath();
+    context.moveTo(marginX,(marginY+marginBetween*4));
+    context.lineTo(marginRight,marginY+marginBetween*4);
+    context.stroke();
+    context.beginPath();
+    context.moveTo(marginRight,marginY+marginBetween*4);
+    context.lineTo(marginX,marginY);
+    context.stroke();
+
+
+    
+
+
+
+
+
+   
+
+   
+
+
+
+
+
+}
