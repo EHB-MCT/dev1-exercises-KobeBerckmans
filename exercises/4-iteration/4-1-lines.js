48f26192b92c1fa53aa6e110aca89a3bf48ef1aa
“Kobe <kobe.berckmans@student.ehb.be>
2022-10-26 11:24:18 +0200

    4-1 lines excercise

---
+++ exercises/4-iteration/4-1-lines.js
@@ -0,0 +1,27 @@
+"use strict";
+import context from "../../scripts/context.js";
+import * as Utils from "../../scripts/utils.js";
+
+let width = context.canvas.width;
+let height = context.canvas.height;
+background();
+function background(){
+context.fillStyle = "#ffa500";
+context.fillRect(0,0,width, height);
+}
+drawLines();
+function drawLines() {
+    let step = 30;
+    context.strokeStyle = "white";
+    let amount = width / step;
+    for(let i= 0; i< amount; i++) {
+        Utils.drawLine(0+step*i, 0,width - step*i, height);
+        
+    }
+    let amountB = height / step;
+    for(let i = 0; i<amountB; i++){
+        Utils.drawLine(0, 0+step*i,width, height - step*i);
+        
+
+    }
+}
